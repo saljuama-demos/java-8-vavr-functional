5af425f77c8ad50e5f1af76bbf12b0f9264ca26a
Salvador Juan Martinez <saljuama@gmail.com>
2018-07-05 02:43:13 +0200

    Predicates 101

--- README.md
+++ README.md
@@ -1,1 +1,54 @@
 # DOJO: JAVA 8 FUNCTIONAL
+
+## Lambdas
+
+A lambda expression is an anonymous function that can be: 
+
+ - assigned to a variable
+ - passed to a method
+ - returned from a method
+
+
+## Functional Interfaces
+
+Any interface with a SAM (Single Abstract Method) is a functional interface, and the recommendation is that they are 
+annotated with the informational `@FunctionalInterface` annotation, as the compiler can emmit an error in case there are
+multiple abstract methods in it, but it is not mandatory. When multiple methods are present in a functional interface, 
+all except one method, MUST provide a default implementation.
+
+The reason of enforcing SAM is because the invocation can be easily replaced with a lambda.
+
+Although it is possible to create your own custom functional interfaces, the Java library already provides, a collection 
+of common functional interfaces, they can be found in the `java.util.function` package, and they come in both 
+generic and primitive specialization flavours (see on the sections below).
+
+The most commonly used functional interfaces are:
+
+### Predicates
+
+This functional interface is used to **evaluate conditions**. Given some input(s), return a boolean.
+ 
+#### Generic format
+
+```java
+@FunctionalInterface
+interface Predicate<T> {
+    boolean test(T input); 
+    default Predicate<T> negate() { ... }
+    default Predicate<T> and(Predicate<T> other) { ... }
+    default Predicate<T> or(Predicate<T> other) { ... }
+}
+```
+
+There is also a `BiPredicate<T,U>` which is the same as `Predicate<T>` but accepting 2 inputs.
+
+#### Primitive Specializations
+
+- `IntPredicate`
+- `LongPredicate`
+- `DoublePredicate`
+
+#### VAVR
+
+VAVR offers some helpers, found in the `io.vavr.Predicates` class, to ease the composition of multiple predicates, and to 
+to use predicates with collections.
---
+++ src/test/java/com/saljuama/dojo/javafunctional/interfaces/PredicatesTest.java
@@ -0,0 +1,108 @@
+package com.saljuama.dojo.javafunctional.interfaces;
+
+import io.vavr.Predicates;
+import io.vavr.collection.List;
+import org.junit.Test;
+
+import java.util.function.*;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+
+public class PredicatesTest {
+
+    @Test
+    public void predicates_are_used_to_test_conditions() {
+        Predicate<Integer> greaterThanZero = input -> input > 0;
+
+        assertTrue(greaterThanZero.test(1));
+        assertFalse(greaterThanZero.test(-1));
+    }
+
+    @Test
+    public void there_are_also_bi_predicates() {
+        BiPredicate<String, Integer> isSizeOf = (string, integer) -> string.length() == integer;
+
+        assertTrue(isSizeOf.test("Hello", 5));
+        assertFalse(isSizeOf.test("World", 10));
+    }
+
+    @Test
+    public void there_are_also_specialized_numeric_primitives_predicates() {
+        IntPredicate isEqualToHundred = input -> input == 100;
+        assertTrue(isEqualToHundred.test(100));
+
+        DoublePredicate isGreaterThanHalf = input -> input > 0.5;
+        assertTrue(isGreaterThanHalf.test(0.6));
+
+        LongPredicate isEqualToBillion = input -> input == 1000000000L;
+        assertFalse(isEqualToBillion.test(10L));
+    }
+
+    @Test
+    public void predicates_can_be_NOT_composed() {
+        Predicate<Integer> greaterThanFive = input -> input > 5;
+        Predicate<Integer> lesserOrEqualThanFive = greaterThanFive.negate();
+
+        assertTrue(lesserOrEqualThanFive.test(4));
+        assertFalse(lesserOrEqualThanFive.test(6));
+    }
+
+    @Test
+    public void predicates_can_be_AND_composed() {
+        Predicate<Integer> greaterThanZero = input -> input > 0;
+        Predicate<Integer> lesserThanTen = input -> input < 10;
+        Predicate<Integer> betweenOneAndNine = greaterThanZero.and(lesserThanTen);
+
+        assertTrue(betweenOneAndNine.test(5));
+        assertFalse(betweenOneAndNine.test(15));
+    }
+
+    @Test
+    public void predicates_can_be_OR_composed() {
+        Predicate<Integer> lesserOrEqualThanZero = input -> input <= 0;
+        Predicate<Integer> greaterOrEqualThanTen = input -> input >= 10;
+        Predicate<Integer> outsideOfRangeZeroToTen = lesserOrEqualThanZero.or(greaterOrEqualThanTen);
+
+        assertTrue(outsideOfRangeZeroToTen.test(-5));
+        assertTrue(outsideOfRangeZeroToTen.test(15));
+        assertFalse(outsideOfRangeZeroToTen.test(5));
+    }
+
+    @Test
+    public void vavr_offers_predicate_composition_helpers() {
+
+        assertTrue(Predicates.noneOf(
+                // need to specify type at least on one when inlined ¯\_(ツ)_/¯
+                (Predicate<String>) input -> input.matches("[aeiouAEIOU]"),
+                input -> input.matches("[A-Z]"),
+                input -> input.matches("[a-z]")
+        ).test(""));
+
+        assertTrue(Predicates.allOf(
+                (Predicate<String>) input -> input.contains(" "),
+                input -> input.matches("[^uU]+"),
+                input -> input.length() > 10
+        ).test("Hello World"));
+
+        assertTrue(Predicates.anyOf(
+                (Predicate<Integer>) input -> input % 2 == 0,
+                input -> input < 0,
+                input -> input == 50
+        ).test(10));
+    }
+
+    @Test
+    public void vavr_offers_predicate_collections_helpers() {
+
+        Predicate<Iterable<Integer>> allArePositive = Predicates.forAll(input -> input > 0);
+        assertTrue(allArePositive.test(List.of(1, 2, 3, 4, 5)));
+
+        Predicate<String> x = input -> input.contains("X");
+        assertFalse(Predicates.exists(x).test(List.of("a", "b", "c", "x", "y", "z")));
+
+        Predicate<String> isVowel = Predicates.isIn("a", "e", "i", "o", "u", "A", "E", "I", "O", "U");
+        assertTrue(isVowel.test("e"));
+    }
+}
