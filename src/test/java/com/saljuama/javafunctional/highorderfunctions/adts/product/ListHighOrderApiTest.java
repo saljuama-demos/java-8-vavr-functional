34321a9c5c3b74b5c62f68de8749941d677f0cd3
Salvador Juan Martinez <saljuama@gmail.com>
2018-07-18 00:10:07 +0200

    product adt list - part 2

--- src/test/java/com/saljuama/javafunctional/highorderfunctions/adts/product/ListHighOrderApiTest.java
+++ src/test/java/com/saljuama/javafunctional/highorderfunctions/adts/product/ListHighOrderApiTest.java
@@ -1,7 +1,8 @@
 package com.saljuama.javafunctional.highorderfunctions.adts.product;
 
+import io.vavr.Tuple;
+import io.vavr.collection.HashMap;
 import io.vavr.collection.List;
-import io.vavr.control.Option;
 import org.junit.Test;
 
 import java.util.function.BiFunction;
@@ -23,9 +24,7 @@ public class ListHighOrderApiTest {
 
         Function<Integer, String> stringifyNumber = x -> x.toString();
 
-        List<String> result = numbers.map(stringifyNumber);
-
-        assertEquals(List.of("1", "2", "3", "4", "5"), result);
+        assertEquals(List.of("1", "2", "3", "4", "5"), numbers.map(stringifyNumber));
     }
 
     @Test
@@ -33,26 +32,30 @@ public class ListHighOrderApiTest {
 
         Function<Integer, List<Integer>> numberAndNext = x -> List.of(x, x + 1);
 
-        List<Integer> result = numbers.flatMap(numberAndNext);
-
-        assertEquals(List.of(1, 2, 2, 3, 3, 4, 4, 5, 5, 6), result);
+        assertEquals(List.of(1, 2, 2, 3, 3, 4, 4, 5, 5, 6), numbers.flatMap(numberAndNext));
     }
 
     @Test
     public void filtering_with_a_predicate_returns_a_list_elements_that_satisfy_the_condition_according_to_the_filter_function() {
-        Predicate<Integer> isEven = x -> x % 2 == 0;
 
-        List<Integer> resultFilter = numbers.filter(isEven);
-        assertEquals(List.of(2, 4), resultFilter);
+        Predicate<Integer> isEvenPredicate = x -> x % 2 == 0;
 
-        List<Integer> resultRemoveAll = numbers.removeAll(isEven);
-        assertEquals(List.of(1, 3, 5), resultRemoveAll);
+        assertEquals(List.of(2, 4), numbers.filter(isEvenPredicate));
 
-        List<Integer> resultRemoveFirst = numbers.removeFirst(isEven);
-        assertEquals(List.of(1, 3, 4, 5), resultRemoveFirst);
+        assertEquals(List.of(1, 3, 5), numbers.removeAll(isEvenPredicate));
 
-        List<Integer> resultRemoveLast = numbers.removeLast(isEven);
-        assertEquals(List.of(1, 2, 3, 5), resultRemoveLast);
+        assertEquals(List.of(1, 3, 4, 5), numbers.removeFirst(isEvenPredicate));
+
+        assertEquals(List.of(1, 2, 3, 5), numbers.removeLast(isEvenPredicate));
+
+    }
+
+    @Test
+    public void find_unique_elements_in_a_list_using_a_discriminator_function() {
+
+        Function<Integer, Boolean> isEvenFunction = x -> x % 2 == 0;
+
+        assertEquals(List.of(1, 2), numbers.distinctBy(isEvenFunction));
     }
 
     @Test
@@ -60,40 +63,103 @@ public class ListHighOrderApiTest {
 
         BiFunction<String, String, String> concatenate = (a, b) -> a + b;
 
-        String result = letters.fold("_", concatenate);
-        assertEquals("_abcde", result);
+        assertEquals("_abcde", letters.fold("_", concatenate));
 
-        String resultLeft = letters.foldLeft("_", concatenate);
-        assertEquals("_abcde", resultLeft);
+        assertEquals("_abcde", letters.foldLeft("_", concatenate));
 
-        String resultRight = letters.foldRight("_", concatenate);
-        assertEquals("abcde_", resultRight);
+        assertEquals("abcde_", letters.foldRight("_", concatenate));
 
-        String resultReduce = letters.reduce(concatenate);
-        assertEquals("abcde", resultReduce);
+        assertEquals("abcde", letters.reduce(concatenate));
     }
 
     @Test
-    public void min_max_values_in_a_list_with_a_function_that_transform_elements_in_the_list_into_comparable_values() {
+    public void min_max_maybe_return_value_from_a_list_using_a_function_for_custom_comparision_between_elements() {
 
-        Function<String, Integer> firstCharToAsciiValue = x -> (int) x.charAt(0);
+        Function<String, Integer> firstCharToAsciiValue = x -> (int) x.charAt(0); // casting char to int returns ascii value
 
-        Option<String> minLetterByAsciiValue = letters.minBy(firstCharToAsciiValue);
-        assertEquals(some("a"), minLetterByAsciiValue);
+        assertEquals(some("a"), letters.minBy(firstCharToAsciiValue));
 
-        Option<String> maxLetterByAsciiValue = letters.maxBy(firstCharToAsciiValue);
-        assertEquals(some("e"), maxLetterByAsciiValue);
-
+        assertEquals(some("e"), letters.maxBy(firstCharToAsciiValue));
 
         Function<String, Integer> firstCharToNegativeAsciiValue = x -> ((int) x.charAt(0)) * -1;
-        Option<String> minLetterByNegativeAsciiValue = letters.minBy(firstCharToNegativeAsciiValue);
-        assertEquals(some("e"), minLetterByNegativeAsciiValue);
 
-        List<String> emptyList = List.empty();
-        Option<String> maxLetterOfEmptyList = emptyList.maxBy(firstCharToAsciiValue);
-        assertEquals(none(), maxLetterOfEmptyList);
+        assertEquals(some("e"), letters.minBy(firstCharToNegativeAsciiValue));
+
+        assertEquals(none(), List.<String>empty().maxBy(firstCharToAsciiValue));
     }
 
-    // TODO: collect peek distinctBy groupBy drop take partition scan sort split zip unzip find
+    @Test
+    public void lists_can_be_partitioned_by_functions_that_evaluate_conditions() {
+
+        Function<String, Boolean> isVowelFunction = x -> x.matches("[aeiouAEIOU]");
+
+        assertEquals(
+                HashMap.of(
+                        true, List.of("a", "e"),
+                        false, List.of("b", "c", "d")
+                ),
+                letters.groupBy(isVowelFunction)
+        );
+
+        Predicate<String> isVowelPredicate = x -> x.matches("[aeiouAEIOU]");
+
+        assertEquals(
+                Tuple.of(
+                        List.of("a", "e"),
+                        List.of("b", "c", "d")
+                ),
+                letters.partition(isVowelPredicate)
+        );
+    }
+
+    @Test
+    public void lists_can_be_split_into_2_by_position_of_an_element_matching_a_predicate() {
+
+        Predicate<String> isVowel = x -> x.matches("[aeiouAEIOU]");
+
+        assertEquals(
+                Tuple.of(
+                        List.of("a"),
+                        List.of("b", "c", "d", "e")
+                ),
+                letters.splitAt(isVowel.negate())
+        );
+
+        assertEquals(
+                Tuple.of(
+                        List.of("a", "b"),
+                        List.of("c", "d", "e")
+                ),
+                letters.splitAtInclusive(isVowel.negate())
+        );
+    }
+
+    @Test
+    public void lists_can_be_sliced_in_different_ways() {
+
+        Predicate<Integer> isFour = x -> x == 4;
+        Predicate<Integer> isNotFour = isFour.negate();
+
+        assertEquals(List.of(1, 2), numbers.take(2));
+        assertEquals(List.of(3, 4, 5), numbers.drop(2));
+
+        assertEquals(List.of(1, 2, 3), numbers.takeUntil(isFour));
+        assertEquals(List.of(4, 5), numbers.dropUntil(isFour));
+
+        assertEquals(List.of(1, 2, 3), numbers.takeWhile(isNotFour));
+        assertEquals(List.of(4,5), numbers.dropWhile(isNotFour));
+
+        assertEquals(List.of(4, 5), numbers.takeRight(2));
+        assertEquals(List.of(1, 2, 3), numbers.dropRight(2));
+
+        // no take right until
+        assertEquals(List.of(1, 2, 3, 4), numbers.dropRightUntil(isFour));
+
+        // no take right while
+        assertEquals(List.of(1, 2, 3, 4), numbers.dropRightWhile(isNotFour));
+
+    }
+
+    // TODO: collect peek scan sort zip unzip find
 
 }
